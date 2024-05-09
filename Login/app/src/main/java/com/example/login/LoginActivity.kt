779291cca84a517c52672e1151fa40b504b4b123
João Marcello <147671563+joaomarcello07@users.noreply.github.com>
2024-05-09 17:10:55 -0300

    Update LoginActivity.kt

--- Login/app/src/main/java/com/example/login/LoginActivity.kt
+++ Login/app/src/main/java/com/example/login/LoginActivity.kt
@@ -15,17 +15,21 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+// Configura o botão de login para verificar as credenciais
         binding.buttonEntrar.setOnClickListener {
             val username = "João"
             val password = "15"
 
+// Verifica se o nome de usuário e a senha estão corretos
             if (username.equals("João") && password.equals("15")) {
                 val i = Intent(this, MainActivity::class.java)
                 i.putExtra("username", username)
-                startActivity(i)
-                finish()
+
+                startActivity(i) // Inicia a tela MainActivity
+                finish() // Encerra a tela de Login
             } else {
                 Toast.makeText(applicationContext, "Errou", Toast.LENGTH_LONG).show()
+ // Aparece uma mensagem de erro se as credenciais estiverem erradas
             }
         }
 
